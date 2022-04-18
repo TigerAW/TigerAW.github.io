4b9f70212633cd7dfe363382086848380b4a202d
TigerAW <adam.wan@hotmail.com>
2022-04-18 01:12:09 +0100

    wellThatWasAnnoying

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 //first JS script
 console.log('Hello World'); 
+
+
+
+let params = new URLSearchParams( window.location.search );
+let searchResult = params.get("searchValue");
+console.log(searchResult);
+document.getElementById("main").innerHTML = searchResult;
+
